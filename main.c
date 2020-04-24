867a9f7d5277faee44924a43038e7715b73ef4c9
ashkan fekri <ashkanfekridev@gmail.com>
2020-04-24 19:23:16 +0430

    First commit

---
+++ main.c
@@ -0,0 +1,10 @@
+// include lib for using in app
+#include <stdio.h>
+
+// create main void from intger type
+int main(){
+	// print Hello Word
+	printf("hello");
+	// return zero number for end void main end exit and die app
+	return 0;
+}
